377ed6ff54959c654855a28b5b74514a13d99e65
Siva Kumar <sivakumar@sivakumars-macbook-air.local>
2018-11-16 17:36:20 +0530

    Alamofire, SwiftyJSON GET method

--- webServices/ViewController.swift
+++ webServices/ViewController.swift
@@ -7,14 +7,50 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgView: UIImageView!
     override func viewDidLoad() {
+        
+      
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+
+      getData()
     }
 
-
+    func getData(){
+        
+        
+        let myURL = "http://bharathiyam.org/RestserviceAPI/Gettempledescription/1/4"
+        
+        Alamofire.request(myURL, method: .get).responseJSON {
+            response in
+            if response.result.isSuccess {
+                
+                let data : JSON = JSON(response.result.value!)
+                let imgData = data["TempleDescriptionResult"]["Gallery"][0]["event_image"].string
+            
+              
+                
+                let myimage : UIImage = UIImage(data: try! Data(contentsOf: URL(string: imgData!)!))!
+                
+              self.imgView.image = myimage
+                
+                
+                print(data)
+                
+                
+            }else {
+                print("error")
+            }
+        }
+        
+        
+    }
 }
 
